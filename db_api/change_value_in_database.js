49a5995ff46e29ac101a621926867a489d91f9de
feldib <beni.feldmayer@gmail.com>
2025-09-05 09:18:35 +0200

    replaced repeating function

--- db_api/change_value_in_database.js
+++ db_api/change_value_in_database.js
@@ -1,147 +1,179 @@
-import dotenv from "dotenv"
-dotenv.config()
-import { createConnection } from "mysql2/promise"
-import { checkIfWishlisted, checkIfFeatured, getQuantityOfArtworkInStock } from "./get_data_from_db.js"
-import { addToShoppingList, addArtworkTags } from './add_to_database.js' 
+import dotenv from "dotenv";
+dotenv.config();
+import { createConnection } from "mysql2/promise";
+import {
+  checkIfWishlisted,
+  checkIfFeatured,
+  getQuantityOfArtworkInStock,
+} from "./get_data_from_db.js";
+import { addToShoppingList, addArtworkTags } from "./add_to_database.js";
+import makeConnection from "../connection.js";
 
-const makeConnection = async () =>
-  createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+const incrementItemInShoppingList = async (user_id, artwork_id, n = 1) => {
+  const connection = await makeConnection();
 
-const incrementItemInShoppingList = async (user_id, artwork_id, n=1) => {
-    const connection = await makeConnection()
-  
-    await connection.query(`
+  await connection.query(
+    `
         UPDATE 
         artworks_in_shopping_list 
         SET quantity = quantity + ?
         WHERE user_id = ? AND artwork_id = ? 
-    `, [n, user_id, artwork_id])
-  
-    connection.end()
-  }
-  
-  const setShoppingCartItemQuantityToZero = async (user_id, artwork_id) => {
-    const connection = await makeConnection()
+    `,
+    [n, user_id, artwork_id]
+  );
 
-    const [quantity_results] = await connection.query(`
+  connection.end();
+};
+
+const setShoppingCartItemQuantityToZero = async (user_id, artwork_id) => {
+  const connection = await makeConnection();
+
+  const [quantity_results] = await connection.query(
+    `
         SELECT quantity FROM artworks_in_shopping_list WHERE user_id = ? AND artwork_id = ? 
-    `, [user_id, artwork_id])
+    `,
+    [user_id, artwork_id]
+  );
 
-    const quantity = quantity_results[0].quantity
-  
-    await connection.query(`
+  const quantity = quantity_results[0].quantity;
+
+  await connection.query(
+    `
         UPDATE artworks SET quantity = quantity + ? WHERE id = ?
-    `, [quantity, artwork_id])
-  
-    await connection.query(`
+    `,
+    [quantity, artwork_id]
+  );
+
+  await connection.query(
+    `
         UPDATE 
         artworks_in_shopping_list 
         SET quantity = 0
         WHERE user_id = ? AND artwork_id = ? 
-    `, [user_id, artwork_id])
-  
-    connection.end()
-  }
-  
-  const decreaseShoppingCartItemQuantity = async (user_id, artwork_id) => {
-    const connection = await makeConnection()
-  
-    const [quantity_results] = await connection.query(`
+    `,
+    [user_id, artwork_id]
+  );
+
+  connection.end();
+};
+
+const decreaseShoppingCartItemQuantity = async (user_id, artwork_id) => {
+  const connection = await makeConnection();
+
+  const [quantity_results] = await connection.query(
+    `
         SELECT quantity FROM artworks_in_shopping_list WHERE user_id = ? AND artwork_id = ? 
-    `, [user_id, artwork_id])
-  
-    const quantity = quantity_results[0].quantity
-  
-    if(quantity>0){
-        await connection.query(`
+    `,
+    [user_id, artwork_id]
+  );
+
+  const quantity = quantity_results[0].quantity;
+
+  if (quantity > 0) {
+    await connection.query(
+      `
           UPDATE artworks SET quantity = quantity+1 WHERE id = ?
-        `, [artwork_id])
-  
-        await connection.query(`
+        `,
+      [artwork_id]
+    );
+
+    await connection.query(
+      `
           UPDATE 
           artworks_in_shopping_list 
           SET quantity = quantity-1
           WHERE user_id = ? AND artwork_id = ? 
-          `, [user_id, artwork_id]
-        )
-    }
-  
-  
-    connection.end()
+          `,
+      [user_id, artwork_id]
+    );
   }
-  
-  const increaseShoppingCartItemQuantity = async (user_id, artwork_id) => {
-    const connection = await makeConnection()
-  
-    const [quantity_results] = await connection.query(`
+
+  connection.end();
+};
+
+const increaseShoppingCartItemQuantity = async (user_id, artwork_id) => {
+  const connection = await makeConnection();
+
+  const [quantity_results] = await connection.query(
+    `
         SELECT quantity FROM artworks WHERE id = ? 
-    `, [artwork_id])
-  
-    const quantity = quantity_results[0].quantity
-  
-    if(quantity>0){
-      await connection.query(`
+    `,
+    [artwork_id]
+  );
+
+  const quantity = quantity_results[0].quantity;
+
+  if (quantity > 0) {
+    await connection.query(
+      `
           UPDATE artworks SET quantity = quantity-1 WHERE id = ?
-      `, [artwork_id])
-  
-      await connection.query(`
+      `,
+      [artwork_id]
+    );
+
+    await connection.query(
+      `
           UPDATE 
           artworks_in_shopping_list 
           SET quantity = quantity+1
           WHERE user_id = ? AND artwork_id = ? 
-      `, [user_id, artwork_id])
-    }else{
-      throw new Error("Item out of stock")
-    }
-  
-    connection.end()
+      `,
+      [user_id, artwork_id]
+    );
+  } else {
+    throw new Error("Item out of stock");
   }
 
-  const resetPassword = async (new_password, email) => {
-    const connection = await makeConnection()
+  connection.end();
+};
+
+const resetPassword = async (new_password, email) => {
+  const connection = await makeConnection();
+  await connection.query(`UPDATE users SET passw = ? WHERE email = ?;`, [
+    new_password,
+    email,
+  ]);
+};
+
+const removeFromWishlisted = async (user_id, artwork_id) => {
+  const connection = await makeConnection();
+  const wishlisted = await checkIfWishlisted(user_id, artwork_id);
+  if (wishlisted) {
     await connection.query(
-          `UPDATE users SET passw = ? WHERE email = ?;`, [new_password, email]
-    )
-  }
-
-  const removeFromWishlisted = async (user_id, artwork_id) => {
-    const connection = await makeConnection()
-    const wishlisted = await checkIfWishlisted(user_id, artwork_id)
-    if(wishlisted){
-      await connection.query(`
+      `
         UPDATE wishlisted SET removed = true WHERE user_id = ? AND artwork_id = ?
-      `, [user_id, artwork_id])
-    }
-  
-    connection.end()
+      `,
+      [user_id, artwork_id]
+    );
   }
 
-  const updateUserData = async (user_id, field_name, value) => {
-    const connection = await makeConnection()
-  
-    if(
-      [
-        "first_name", "last_name", "email", "address", "phone_number"
-      ].includes(field_name)
-    ){
-      await connection.query(`
+  connection.end();
+};
+
+const updateUserData = async (user_id, field_name, value) => {
+  const connection = await makeConnection();
+
+  if (
+    ["first_name", "last_name", "email", "address", "phone_number"].includes(
+      field_name
+    )
+  ) {
+    await connection.query(
+      `
         UPDATE users SET ${field_name} = ? WHERE id = ?
-      `, [value, user_id])
-    }
-  
-    connection.end()
+      `,
+      [value, user_id]
+    );
   }
 
-  const updateArtworkTags = async (artwork_id, tags) => {
-    const connection = await makeConnection()
+  connection.end();
+};
 
-    const [tags_of_artwork] = await connection.query(`
+const updateArtworkTags = async (artwork_id, tags) => {
+  const connection = await makeConnection();
+
+  const [tags_of_artwork] = await connection.query(
+    `
         SELECT 
           artwork_tags.id as artwork_tag_id, 
           tags.id as tag_id, 
@@ -150,140 +182,169 @@ const incrementItemInShoppingList = async (user_id, artwork_id, n=1) => {
         LEFT JOIN tags
         ON tags.id = artwork_tags.tag_id
         WHERE artwork_id = ? AND artwork_tags.removed = false
-      `, [artwork_id]) 
+      `,
+    [artwork_id]
+  );
 
-    const tagsToAdd = tags.filter(
-      (tag) => !tags_of_artwork.some(
-          tg => tg.tname === tag
-        )
-    )
+  const tagsToAdd = tags.filter(
+    (tag) => !tags_of_artwork.some((tg) => tg.tname === tag)
+  );
 
-    console.log("tagsToAdd: ", JSON.stringify(tagsToAdd))
+  console.log("tagsToAdd: ", JSON.stringify(tagsToAdd));
 
-    await addArtworkTags(artwork_id, tagsToAdd)
+  await addArtworkTags(artwork_id, tagsToAdd);
 
-    const tagsToRemove = tags_of_artwork.filter(
-      (tg) => !tags.includes(tg.tname)
-    ) 
+  const tagsToRemove = tags_of_artwork.filter((tg) => !tags.includes(tg.tname));
 
-
-    await Promise.all(tagsToRemove.map(async (tag) => {
-
-      await connection.query(`
+  await Promise.all(
+    tagsToRemove.map(async (tag) => {
+      await connection.query(
+        `
         UPDATE artwork_tags SET removed = true WHERE id = ?
-      `, [tag.artwork_tag_id]) 
-    }))
+      `,
+        [tag.artwork_tag_id]
+      );
+    })
+  );
 
-    connection.end()
-  }
+  connection.end();
+};
 
-  const updateArtworkData = async (artwork_id, field_name, value) => {
- 
-    if(
-      [
-        "title", "artist_name", "price", "quantity", "descript", "category_id"
-      ].includes(field_name)
-    ){
-      const connection = await makeConnection()
+const updateArtworkData = async (artwork_id, field_name, value) => {
+  if (
+    [
+      "title",
+      "artist_name",
+      "price",
+      "quantity",
+      "descript",
+      "category_id",
+    ].includes(field_name)
+  ) {
+    const connection = await makeConnection();
 
-      await connection.query(`
+    await connection.query(
+      `
         UPDATE artworks SET ${field_name} = ? WHERE id = ?
-      `, [value, artwork_id])
+      `,
+      [value, artwork_id]
+    );
 
-      connection.end()
-    }else if("tags" === field_name){
+    connection.end();
+  } else if ("tags" === field_name) {
+    await updateArtworkTags(
+      artwork_id,
+      value.map((tag) => {
+        return tag.tname;
+      })
+    );
+  }
+};
 
-      await updateArtworkTags(artwork_id, value.map((tag)=>{
-        return tag.tname
-      }))
-
-    }
-  }  
-
-  const approveReview = async (id) =>{
-    const connection = await makeConnection()
-    await connection.query(`
+const approveReview = async (id) => {
+  const connection = await makeConnection();
+  await connection.query(
+    `
         UPDATE reviews SET approved = true where id = ?
-    `, [id])
-  
-    connection.end()
-  }
-  
-  const removeReview = async (id) =>{
-    const connection = await makeConnection()
-    await connection.query(`
+    `,
+    [id]
+  );
+
+  connection.end();
+};
+
+const removeReview = async (id) => {
+  const connection = await makeConnection();
+  await connection.query(
+    `
         UPDATE reviews SET removed = true where id = ?
-    `, [id])
-  
-    connection.end()
-  }
+    `,
+    [id]
+  );
 
-  const removeFromFeatured = async (artwork_id) => {
-    const connection = await makeConnection()
-    const featured = await checkIfFeatured(artwork_id)
-    if(featured){
-      await connection.query(`
+  connection.end();
+};
+
+const removeFromFeatured = async (artwork_id) => {
+  const connection = await makeConnection();
+  const featured = await checkIfFeatured(artwork_id);
+  if (featured) {
+    await connection.query(
+      `
         UPDATE featured SET removed = true WHERE artwork_id = ?
-      `, [artwork_id])
-    }
-  
-    connection.end()
+      `,
+      [artwork_id]
+    );
   }
 
-  const removeArtwork = async (artwork_id) => {
-    const connection = await makeConnection()
-    await connection.query(`
+  connection.end();
+};
+
+const removeArtwork = async (artwork_id) => {
+  const connection = await makeConnection();
+  await connection.query(
+    `
         UPDATE artworks SET removed = true where id = ?
-    `, [artwork_id])
+    `,
+    [artwork_id]
+  );
 
-    await connection.query(`
+  await connection.query(
+    `
         UPDATE artwork_tags SET removed = true where artwork_id = ?
-    `, [artwork_id])
+    `,
+    [artwork_id]
+  );
 
-  
-    connection.end()
-  }
+  connection.end();
+};
 
-  const replaceSavedShoppingCart = async (user_id, shopping_cart) => {
-    const connection = await makeConnection()  
+const replaceSavedShoppingCart = async (user_id, shopping_cart) => {
+  const connection = await makeConnection();
 
-    const [res] = await connection.query(`
+  const [res] = await connection.query(
+    `
       SELECT artwork_id FROM artworks_in_shopping_list WHERE user_id = ? AND quantity > 0
-    `, [user_id])
-    
-    const ids = [... new Set(
-      res.map( obj => obj.artwork_id )
-    )]
+    `,
+    [user_id]
+  );
 
-    await Promise.all(ids.map(async (artw_id) => {
-      await setShoppingCartItemQuantityToZero(user_id, artw_id)
-    }))
+  const ids = [...new Set(res.map((obj) => obj.artwork_id))];
 
-    await Promise.all(shopping_cart.map(async (item) => {
-      const quantity_in_stock = await getQuantityOfArtworkInStock(item.artwork_id)
+  await Promise.all(
+    ids.map(async (artw_id) => {
+      await setShoppingCartItemQuantityToZero(user_id, artw_id);
+    })
+  );
 
-      const quantity = item.quantity > quantity_in_stock ? quantity_in_stock : item.quantity
+  await Promise.all(
+    shopping_cart.map(async (item) => {
+      const quantity_in_stock = await getQuantityOfArtworkInStock(
+        item.artwork_id
+      );
 
-      addToShoppingList(user_id, item.artwork_id, quantity)
+      const quantity =
+        item.quantity > quantity_in_stock ? quantity_in_stock : item.quantity;
 
-    }))
-  
-    connection.end()
-  }
+      addToShoppingList(user_id, item.artwork_id, quantity);
+    })
+  );
 
+  connection.end();
+};
 
 export {
-    setShoppingCartItemQuantityToZero,
-    decreaseShoppingCartItemQuantity,
-    increaseShoppingCartItemQuantity,
-    incrementItemInShoppingList,
-    resetPassword,
-    removeFromWishlisted,
-    updateUserData,
-    approveReview,
-    removeReview,
-    removeFromFeatured,
-    removeArtwork,
-    replaceSavedShoppingCart,
-    updateArtworkData
-}
+  setShoppingCartItemQuantityToZero,
+  decreaseShoppingCartItemQuantity,
+  increaseShoppingCartItemQuantity,
+  incrementItemInShoppingList,
+  resetPassword,
+  removeFromWishlisted,
+  updateUserData,
+  approveReview,
+  removeReview,
+  removeFromFeatured,
+  removeArtwork,
+  replaceSavedShoppingCart,
+  updateArtworkData,
+};
